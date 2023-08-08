9f04f4472ebeff4a6d632218002b0c54f55119d8
Daniel Caro <danielcarogutierrez@gmail.com>
2023-08-08 09:24:35 -0500

    Se crea documentacion swagger ademas de la documentacion de la los claims

--- src/main/java/com/example/restaurantepragma/controllers/CustomerController.java
+++ src/main/java/com/example/restaurantepragma/controllers/CustomerController.java
@@ -4,6 +4,11 @@ import com.example.restaurantepragma.dto.customer.CustomerDTO;
 import com.example.restaurantepragma.dto.customer.CustomerErrorDTO;
 import com.example.restaurantepragma.entities.Customer;
 import com.example.restaurantepragma.services.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +28,18 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+
+    @Operation(summary = "Guarda un nuevo cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente creado exitosamente",
+                    content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Customer.class)) }),
+            @ApiResponse(responseCode = "400", description = "Error al guardar el cliente",
+                    content = @Content)
+    })
+
     // Manejo de solicitudes HTTP POST para guardar un cliente
+
     @PostMapping("/")
     public ResponseEntity<CustomerDTO> save(@RequestBody Customer customer)throws Exception{
         try {
@@ -35,6 +51,15 @@ public class CustomerController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new CustomerErrorDTO(e.getMessage()));
         }
     }
+
+    @Operation(summary = "Obtener todos los clientes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente obtenidos exitosamente",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)) }),
+            @ApiResponse(responseCode = "400", description = "Error al obtener clientes",
+                    content = @Content)
+    })
 
     // Manejo de solicitudes HTTP GET para obtener todos los clientes
     @GetMapping("/")
@@ -49,6 +74,15 @@ public class CustomerController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(customerDTOS);
         }
     }
+    @Operation(summary = "Tener Registros de un cliente por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registros de cliente obtenidos exitosamente",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)) }),
+            @ApiResponse(responseCode = "400", description = "Error al obtener registros del cliente",
+                    content = @Content)
+    })
+
     @GetMapping("/logs{id}")
     public ResponseEntity<CustomerDTO> findLogsById(@PathVariable Long id){
         try {
